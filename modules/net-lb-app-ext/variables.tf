f4e280a75699373430d579be8c1bbd3d5e3baf5d
Kővágó Ádám <adam.kovago@ysys.hu>
2025-06-10 17:28:55 +0200

    refactor: update project_id handling in serverless NEG resource and add optional project_id variable

--- modules/net-lb-app-ext/variables.tf
+++ modules/net-lb-app-ext/variables.tf
@@ -163,6 +163,7 @@ variable "neg_configs" {
       network        = optional(string)
       subnetwork     = optional(string)
     }))
+    project_id = optional(string, null)
   }))
   default  = {}
   nullable = false

372da13a199403d921b28409f39cb926989192a3
Kővágó Ádám <adam.kovago@ysys.hu>
2025-07-29 11:12:01 +0200

    Made it look like the same as Fabric

--- modules/net-lb-app-ext/variables.tf
+++ modules/net-lb-app-ext/variables.tf
@@ -166,7 +166,6 @@ variable "neg_configs" {
       network        = optional(string)
       subnetwork     = optional(string)
     }))
-    project_id = optional(string, null)
   }))
   default  = {}
   nullable = false
